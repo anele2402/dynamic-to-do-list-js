eb41fc03dd03c09d20ac840bfe6573e1aa3c07c5
Anele <anelenkomolity24@gmail.com>
2025-06-22 10:19:40 +0200

    created a dynamic to do list

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function addTask() {
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+    let taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = "Remove";
+        removeBtn.className = 'remove-btn';
+
+        removeBtn.onclick = function () {
+            taskList.removeChild(li);
+        };
+
+
+        li.appendChild(removeBtn);
+        taskList.appendChild(li);
+
+        taskInput.value = '';
+    } else {
+        alert("Enter a task");
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+
+    addButton.addEventListener('click', addTask);
+
+    taskInput.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    });
+    addTask();
+});
